a184df4591f39eba33817f058057a3ebd1b4d488
jbacon30 <jmarcusb95@gmail.com>
2017-11-28 10:42:54 -0500

    Location change for Init Mvmnt
    "Init Movement placed after postcalc and before northside sweep. - Group"

--- ProjectSolution.asm
+++ ProjectSolution.asm
@@ -491,17 +491,7 @@ CapVelLow:
 	CALL    Neg
 	RETURN
 	MaxVal: DW 510
-InitialMovement:
-	IN		Dist3
-	STORE	Temp
-	IN		DIST2
-	ADD		Temp
-	SHIFT	-1
-	LOAD	StartingWWall
-	SUB		Temp
-	JNEG	InitialMovement
-	JZERO	StateSwitch
-	JPOS	StateSwitch
+
 ;***************************************************************
 ;* Subroutines
 ;***************************************************************
@@ -817,7 +807,19 @@ PostCalcWaitCycle:
 	JPOS   PostCalcWaitCycle ; not ready (KEYs are active-low, hence JPOS)
 	LOAD   Zero
 	OUT    XLEDS       ; clear LEDs once ready to continue
-	JUMP	StateSwitch
+	
+InitialMovement:
+	IN		Dist3
+	STORE	Temp
+	IN		DIST2
+	ADD		Temp
+	SHIFT	-1
+	LOAD	StartingWWall
+	SUB		Temp
+	JNEG	InitialMovement
+	JZERO	StateSwitch
+	JPOS	StateSwitch
+
 	
 NorthsideSweep:
 	;TO-DO
